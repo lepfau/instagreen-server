8910a4c13d54eed2b11f7d24b2ddf8597cdb2523
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-01-21 10:28:14 +0100

    first deploy

---
+++ public/precache-manifest.e26ee8d091ab41e869ef1f5cc7a24e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d14b75eb2a9d62eccbe52b0885b163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb27b47e2c418c11d3b9",
+    "url": "/static/css/main.797b465b.chunk.css"
+  },
+  {
+    "revision": "a9d5feb7ade78666d974",
+    "url": "/static/js/2.84f1b037.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.84f1b037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb27b47e2c418c11d3b9",
+    "url": "/static/js/main.9cb0024d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
