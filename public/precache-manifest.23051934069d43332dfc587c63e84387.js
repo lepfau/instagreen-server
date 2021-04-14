dec81dfc0b111ffe9eb7cc57f886a024f438b81d
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-04-14 14:04:47 +0200

    logique my plants ok

---
+++ public/precache-manifest.23051934069d43332dfc587c63e84387.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9db55e8e264d3e7389755f61cb17e4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94548f73820b775382a4",
+    "url": "/static/css/main.02a272a3.chunk.css"
+  },
+  {
+    "revision": "701e97ebfb01ad218a6c",
+    "url": "/static/js/2.c3d2a414.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c3d2a414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94548f73820b775382a4",
+    "url": "/static/js/main.3826e3fd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8f6d93bcdaebe9333be1a59c74ce3af3",
+    "url": "/static/media/loadinggif.8f6d93bc.gif"
+  },
+  {
+    "revision": "4a35f91497b46f5095ac0d69ad41a17a",
+    "url": "/static/media/loadingwall.4a35f914.gif"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d6927734cc826d7023a0987dff1da686.js"
+  "/precache-manifest.23051934069d43332dfc587c63e84387.js"
 );
 
 self.addEventListener('message', (event) => {
