5450a32b32a846a8a024ffe5151fdcecd4dfc2a7
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-04-14 10:44:56 +0200

    meilleur css et preview image upload ok

---
+++ public/precache-manifest.c615f4e0758953e68d0cf5cbcc0010f1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03a6e70f57138e07c9bc4e847f7d1c7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8be61c4ee7f298f18002",
+    "url": "/static/css/main.bac1a795.chunk.css"
+  },
+  {
+    "revision": "701e97ebfb01ad218a6c",
+    "url": "/static/js/2.c3d2a414.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c3d2a414.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be61c4ee7f298f18002",
+    "url": "/static/js/main.a28cc44c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "8f6d93bcdaebe9333be1a59c74ce3af3",
+    "url": "/static/media/loadinggif.8f6d93bc.gif"
+  },
+  {
+    "revision": "4a35f91497b46f5095ac0d69ad41a17a",
+    "url": "/static/media/loadingwall.4a35f914.gif"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f99434b8f998840b02e28b0b816bde7.js"
+  "/precache-manifest.c615f4e0758953e68d0cf5cbcc0010f1.js"
 );
 
 self.addEventListener('message', (event) => {
