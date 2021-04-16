a45778d6c025687ac6b975bd2b6f3dc71688b10e
Edouard Pfauwadel <edouard.pfauwadel@gmail.com>
2021-04-16 14:58:04 +0200

    item model new ok

--- models/Item.js
+++ models/Item.js
@@ -4,20 +4,14 @@ const Schema = mongoose.Schema;
 const itemSchema = new Schema(
   {
     name: String,
-    description: String,
     image: {
       type: String,
       default:
         "https://cdn1.iconfinder.com/data/icons/gardening-filled-line/614/1935_-_Growing_Plant-512.png",
     },
-    category: [
-      {
-        type: String,
-        enum: ["Plant"],
-      },
-    ],
+
     quantity: Number,
-    contact: String,
+    phone: String,
     location: {
       type: {
         type: String,
